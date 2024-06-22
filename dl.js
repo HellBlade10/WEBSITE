798bcc94ab58909754dbb9236b84544e16ae5abc
HellBlade10 <103781988+HellBlade10@users.noreply.github.com>
2024-06-22 11:31:44 +0545

    added a device-width

--- dl.js
+++ dl.js
@@ -1,12 +1,3 @@
-function showTime() {
-	document.getElementById('currentTime').innerHTML = new Date().toLocaleTimeString();
-}
-showTime();
-setInterval(function () {
-	showTime();}, 1000);
-function showDate() {
-        document.getElementById('currentDate').innerHTML = new Date().toLocaleDateString();
-    }
-    showDate();
-    setInterval(function () {
-        showDate();});
+function popup(){
+    document.getElementById('')=alert('hi');
+};
